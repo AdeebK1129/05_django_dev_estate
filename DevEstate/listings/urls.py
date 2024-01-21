b953c959b6f4bc2013ba4879dde51636b7218781
Adeeb <adeeb19909@gmail.com>
2024-01-20 19:52:20 -0500

    initiliazed functional property details pages

--- DevEstate/listings/urls.py
+++ DevEstate/listings/urls.py
@@ -5,4 +5,5 @@ app_name = 'listings'
 
 urlpatterns = [
     path('', views.property_list, name='property_list'),
+    path('property_detail/<int:zpid>/', views.property_detail, name='property_detail'),
 ]
