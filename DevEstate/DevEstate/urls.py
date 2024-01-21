4182d7165e6b82f042cf0cc6564ad3281dc0388a
Samuel Li <xli40@stuy.edu>
2024-01-21 16:20:44 -0500

    redirected home page

--- DevEstate/DevEstate/urls.py
+++ DevEstate/DevEstate/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/main/')),
     path('main/', include('main.urls')), 
     path('listings/', include('listings.urls', namespace='listings')),
 ]
