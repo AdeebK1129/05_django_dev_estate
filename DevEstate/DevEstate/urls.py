7aadc74d136b074c0d853099176dbefaf40f2519
Samuel Li <xli40@stuy.edu>
2024-01-21 16:11:12 -0500

    created and designed homepage/main app

--- DevEstate/DevEstate/urls.py
+++ DevEstate/DevEstate/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', include('main.urls')), 
     path('listings/', include('listings.urls', namespace='listings')),
 ]
